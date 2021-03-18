528357393043d97b18bbb7860131901ac4f4bf2f
Luiz S.S. Baglie <luizssb.biz@gmail.com>
2021-03-17 22:18:10 -0300

    Aula do dia 17/03

--- app/src/main/java/com/example/aulaite/MainActivity.kt
+++ app/src/main/java/com/example/aulaite/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aulaite
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -48,6 +50,28 @@ class MainActivity : AppCompatActivity() {
         button_multiply.setOnClickListener(handleOperationClick { a, b -> a * b })
 
         button_divide.setOnClickListener(handleOperationClick { a, b -> a / b })
+    }
+
+    private fun checkOutResources() {
+        val appName = this.getString(R.string.app_name) // Aula ITE
+        val helloLuiz = getString(R.string.label_hello_word, "Luiz") // Olá Luiz!
+        val hello = getString(R.string.label_hello_word) // Olá %s!
+        val color = ContextCompat.getColor(this, R.color.colorPrimary)
+
+        val stringArray = resources.getStringArray(R.array.options_signup_username) // [solteirolegal, casalcurioso, cachorros_da_trevas]
+        val quantityProducts = resources.getQuantityString(
+            R.plurals.quantity_cart_products,
+            3,
+            3
+        ) // Você selecionou 3 produtos
+
+        val marginDefault = resources.getDimensionPixelSize(R.dimen.margin_default)
+        val intArray = resources.getIntArray(R.array.array_integer_general)
+        val boolVal = resources.getBoolean(R.bool.control_main_open_profile)
+
+        val view = layoutInflater.inflate(R.layout.activity_main, null, false)
+        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_launcher_background)
+        val mipmap = ContextCompat.getDrawable(this, R.mipmap.ic_launcher)
     }
 
     // Float -> Float?
